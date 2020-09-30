4663248c57958d59ed770e2a6c5f69edd1d9c59a
tpepperm <tkarpukova@gmail.com>
2020-09-30 20:47:34 +0300

    init: start typical-arrays-problems task and commit all three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    var minimum = Number.MAX_SAFE_INTEGER;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] < minimum) {
+        minimum = array[i];
+      }
+    }
+    return minimum;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    var maximum = Number.MIN_SAFE_INTEGER;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > maximum) {
+        maximum = array[i];
+      }
+    }
+    return maximum;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    var average = 0;
+    for (var i = 0; i < array.length; i++) {
+      average += array[i];
+    }
+    return (average / array.length);
+  } else {
+    return 0;
+  }
 }
